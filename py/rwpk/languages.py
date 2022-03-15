a63c6de74a24d037578d09209cd65deb49180112
siphr <haris@sscriptureofman.com>
2022-03-15 02:16:09 +0000

    fixing  language > file interpolation

--- py/rwpk/languages.py
+++ py/rwpk/languages.py
@@ -9,7 +9,7 @@ def get_language_pool(language, exe_path):
     if language.lower() == 'urdu': encoding = 'utf-16'
     elif language.lower() == 'sindhi': encoding = 'utf-8'
 
-    file_path = '{}/{}/{}.txt'.format(exe_path, _DATA, language)
+    file_path = '{}/{}/{}.txt'.format(exe_path, _DATA, language.lower())
 
     if not os.path.exists(file_path):
         raise('Please make sure that the language file exists.')
